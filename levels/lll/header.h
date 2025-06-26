d6e3d0ddf62cb392b85b99f74b923348cb1c4cfa
MushJam64 <100545041+MushJam64@users.noreply.github.com>
2025-06-26 15:14:21 -0400

    i may be stupid and forgot to do large texture stuff

---
+++ levels/lll/header.h
@@ -0,0 +1,11 @@
+#ifndef LLL_HEADER_H
+#define LLL_HEADER_H
+
+#include "types.h"
+#include "game/moving_texture.h"
+
+extern const LevelScript level_lll_entry[];
+
+
+#include "levels/lll/header.inc.h"
+#endif
